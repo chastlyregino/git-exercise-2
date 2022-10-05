4d832245363e2dbf4ebe4cacfa7608ea1f93de5d
Chastly Regino <chastly_regino@icloud.com>
2022-10-05 09:24:15 +0800

    Avoiding return statement in arrow functions

--- lib.js
+++ lib.js
@@ -1,15 +1,7 @@
-const square = (n) => {
-    return n * n;
-}
+const square = (n) => n * n;
 
-const fahrenheit = (celsius) => {
-    return celsius * 9 / 5 + 32;
-}
+const fahrenheit = (celsius) => celsius * 9 / 5 + 32;
 
-const rectangleArea = (a, b) => {
-    return a * b;
-}
+const rectangleArea = (a, b) => a * b;
 
-const isPalindrome = (str) => {
-    return str.toLowerCase() === str.split('').reverse().join('').toLowerCase();
-}
+const isPalindrome = (str) => str.toLowerCase() === str.split('').reverse().join('').toLowerCase();
