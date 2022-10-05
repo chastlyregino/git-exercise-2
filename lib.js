403f802bf4526f236826063e3ec81948e2254503
Finn Manapat <113877935+chastlyregino@users.noreply.github.com>
2022-10-05 09:43:17 +0800

    Created 4 functions with String prototype - Finn (#1)
    Created 4 functions with String prototype

---
+++ lib.js
@@ -0,0 +1,7 @@
+const square = (n) => n * n;
+
+const fahrenheit = (celsius) => celsius * 9 / 5 + 32;
+
+const rectangleArea = (a, b) => a * b;
+
+const isPalindrome = (str) => str.toLowerCase() === str.split('').reverse().join('').toLowerCase();
