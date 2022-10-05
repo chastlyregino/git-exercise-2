e61475fbe287e086b510be4633155f3649d017cb
Chastly Regino <chastly_regino@icloud.com>
2022-10-05 10:11:19 +0800

    module.exports with multiple functions exercise

---
+++ index.js
@@ -0,0 +1,10 @@
+const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib.js');
+
+console.log(square(4));
+
+console.log(fahrenheit(60));
+
+console.log(rectangleArea(4, 5));
+
+console.log(isPalindrome('Bob'));
+console.log(isPalindrome('Alice'));
--- lib.js
+++ lib.js
@@ -5,3 +5,10 @@ const fahrenheit = (celsius) => celsius * 9 / 5 + 32;
 const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => str.toLowerCase() === str.split('').reverse().join('').toLowerCase();
+
+module.exports = {
+    square,
+    fahrenheit,
+    rectangleArea,
+    isPalindrome
+}
