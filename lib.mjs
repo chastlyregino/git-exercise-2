7e187610d755ece13a0058352ccad3dc9af70d26
Chastly Regino <chastly_regino@icloud.com>
2022-10-26 11:09:03 +0800

    Johnny wanted the return for isPalindrome to be one line

--- lib.mjs
+++ lib.mjs
@@ -4,5 +4,6 @@ export const fahrenheit = (celsius) => (celsius * 9) / 5 + 32
 
 export const rectangleArea = (a, b) => a * b
 
-export const isPalindrome = (str) => str.toLowerCase()
-=== str.split('').reverse().join('').toLowerCase()
+export const isPalindrome = (str) => (
+  str.toLowerCase() === str.split('').reverse().join('').toLowerCase()
+)
