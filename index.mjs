fad1155d43c3c0411d3e817bc2593915c4e9d05f
Chastly Regino <chastly_regino@icloud.com>
2022-10-26 10:47:34 +0800

    Replaced prettier with eslint and used ESM imports

---
+++ index.mjs
@@ -0,0 +1,10 @@
+import * as lib from './lib.mjs'
+
+console.log(lib.square(4))
+
+console.log(lib.fahrenheit(60))
+
+console.log(lib.rectangleArea(4, 5))
+
+console.log(lib.isPalindrome('Bob'))
+console.log(lib.isPalindrome('Alice'))
